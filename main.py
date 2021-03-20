997839e8cd8116b6bf37ef8506bc6dc30991ceed
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2021-03-20 23:26:04 +0700

    Update main

--- main.py
+++ main.py
@@ -1,81 +1,17 @@
-from fractions import Fraction
-from collections import Counter
-import sys
-
-
-def arithmetic_encode(string: str) -> Fraction:
-    """
-    Function to encode a string using arithmetic coding.
-
-    :param string: the input string to be encoded
-    :return: object of class Fraction (common fraction)
-    """
-    freq_dict = Counter(string)
-    segments = {}
-    counter = Fraction()
-    str_len = len(string)
-
-    for char, freq in list(freq_dict.items()):
-        probability = Fraction(freq, str_len)
-        segments[char] = (counter, counter + probability)
-        counter += probability
-
-    left = Fraction()
-    right = Fraction(1, 1)
-
-    point = Fraction()
-
-    for char in string:
-        new_right = left + (right - left) * segments[char][1]
-        new_left = left + (right - left) * segments[char][0]
-        right = new_right
-        left = new_left
-        point = (left)
-
-    return point
-
-
-def arithmetic_decode(code: Fraction, freq_dict: dict) -> str:
-    """
-    A function to decode an encoded message using arithmetic coding.
-
-    :param code: the code of the encoded message.
-    :param freq_dict: dictionary with the frequencies of characters in the encoded string
-    :return: decoded string
-    """
-
-    str_len = sum(freq_dict.values())
-    segments = []
-    decoded_string = ""
-
-    counter = Fraction()
-    for char, freq in list(freq_dict.items()):
-        prob = Fraction(freq, str_len)
-        segments.append((counter, counter + prob, char))
-        counter += prob
-
-    for _ in range(str_len):
-        for left, right, char in segments:
-            if left <= code < right:
-                decoded_string += char
-                code = (code - left) / (right - left)
-                break
-
-    return decoded_string
+import arithmetic as arith
 
 
 def main() -> None:
-    string = sys.argv[1]
-    print(f"Input string: {string}")
+    """Main function"""
+    string = input("Enter the string: ")
 
-    freq_dict = Counter(string)
-    encode = arithmetic_encode(string)
+    freq_dict, code = arith.encode(string)
     print(f"\nEncoded message:")
-    print(f"(common) {encode.numerator} / {encode.denominator}")
-    print(f"(decimal) {float(encode):.30e}\n")
+    print(f"(common) {code.numerator} / {code.denominator}")
+    print(f"(decimal) {float(code):.30e}\n")
 
-    decode = arithmetic_decode(encode, freq_dict)
-    print(f"Decoded message is '{decode}'")
+    decoded_string = arith.decode(code, freq_dict)
+    print(f"Decoded message is '{decoded_string}'")
 
 
 if __name__ == "__main__":
